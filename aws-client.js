a2e4e07a6464a24236aa02aed5775b31704b4e82
kaylakantola <kkantola@trhc.com>
2020-05-26 12:39:21 -0400

    fixes async issue

--- aws-client.js
+++ aws-client.js
@@ -7,18 +7,9 @@ const getSecret = async (secretName) => {
   const params = {
     Name: secretName,
   };
-  try {
-    const result = await ssm.getParameter(params).promise();
-    console.log("before resolved:", { result }); // i would expect this to initially log as "undefined" because itll log before the promise resolves
 
-    if (result) {
-      console.log("after resolved:", { result }); // i would expect this to have what you want
-      return result.Parameter.Value;
-    }
-  } catch (e) {
-    console.log("error from inside getSecret", { e });
-    throw new Error(e);
-  }
+  const result = await ssm.getParameter(params).promise();
+  return result.Parameter.Value;
 };
 
 module.exports = { getSecret };
--- server.js
+++ server.js
@@ -1,109 +1,114 @@
 const Eris = require("eris");
-const awsClient = require("./aws-client.js");
-const secret = awsClient
-  .getSecret("lucretia-bott-token")
-  .then((password) => password)
-  .catch((error) => console.log({ error }));
+const { getSecret } = require("./aws-client.js");
 
-console.log({ secret });
-const bot = new Eris.Client(token);
+const runServer = async () => {
+  try {
+    const secret = await getSecret("lucretia-bott-token");
+    console.log("now thats what i call secret!", { secret });
+    const bot = new Eris.Client(secret);
 
-const PREFIX = "!";
-const BOT_OWNER_ID = "254422154295115776";
-const PREMIUM_CUTOFF = 10;
-const commandMap = {};
+    const PREFIX = "!";
+    const BOT_OWNER_ID = "254422154295115776";
+    const PREMIUM_CUTOFF = 10;
+    const commandMap = {};
 
-const premiumRole = {
-  name: "Premium Member",
-  color: 0x6aa84f,
-  hoist: true,
-};
+    const premiumRole = {
+      name: "Premium Member",
+      color: 0x6aa84f,
+      hoist: true,
+    };
 
-//
-// commandMap['trivia'] = {
-//   execute: (msg, args) => {
-//
-//   }
-// }
+    //
+    // commandMap['trivia'] = {
+    //   execute: (msg, args) => {
+    //
+    //   }
+    // }
 
-commandMap["addpayment"] = {
-  botOwnerOnly: true,
-  execute: (msg, args) => {
-    const mention = args[0];
-    const amount = parseFloat(args[1]);
-    const guild = msg.channel.guild;
-    const userId = mention.replace(/<@(.*?)>/, (match, group1) => group1);
-    const member = guild.members.find((member) => {
-      return member.username === userId;
+    commandMap["addpayment"] = {
+      botOwnerOnly: true,
+      execute: (msg, args) => {
+        const mention = args[0];
+        const amount = parseFloat(args[1]);
+        const guild = msg.channel.guild;
+        const userId = mention.replace(/<@(.*?)>/, (match, group1) => group1);
+        const member = guild.members.find((member) => {
+          return member.username === userId;
+        });
+
+        const userIsInGuild = !!member;
+        if (!userIsInGuild) {
+          return msg.channel.createMessage("User not found in this channel.");
+        }
+
+        const amountIsValid = amount && !Number.isNaN(amount);
+        if (!amountIsValid) {
+          return msg.channel.createMessage("Invalid donation amount");
+        }
+
+        return Promise.all([
+          msg.channel.createMessage(`${mention} paid $${amount.toFixed(2)}`),
+          updateMemberRoleForDonation(guild, member, amount),
+        ]);
+      },
+    };
+
+    async function updateMemberRoleForDonation(guild, member, donationAmount) {
+      if (guild && member && donationAmount >= PREMIUM_CUTOFF) {
+        let role = Array.from(guild.roles.values()).find(
+          (role) => role.name === premiumRole.name
+        );
+        if (!role) {
+          role = await guild.createRole(premiumRole);
+        }
+
+        return member.addRole(role.id, "Donated $10 or more.");
+      }
+    }
+
+    // command handler, handles 'no matching command' case
+    bot.on("messageCreate", async (msg) => {
+      const content = msg.content;
+      if (!content.startsWith(PREFIX)) {
+        return;
+      }
+
+      const parts = content
+        .split(" ")
+        .map((s) => s.trim())
+        .filter((s) => s);
+      const commandName = parts[0].substr(PREFIX.length);
+
+      const command = commandMap[commandName];
+      if (!command) {
+        return;
+      }
+
+      const authorIsBotOwner = msg.author.id === BOT_OWNER_ID;
+      if (command.botOwnerOnly && !authorIsBotOwner) {
+        return await msg.channel.createMessage(
+          `Hey, fuck you. You are ${msg.author.id}.`
+        );
+      }
+
+      const args = parts.slice(1);
+
+      try {
+        await command.execute(msg, args);
+      } catch (err) {
+        console.warn("Error handling command");
+        console.warn(err);
+      }
     });
 
-    const userIsInGuild = !!member;
-    if (!userIsInGuild) {
-      return msg.channel.createMessage("User not found in this channel.");
-    }
+    bot.on("error", (err) => {
+      console.warn(err);
+    });
 
-    const amountIsValid = amount && !Number.isNaN(amount);
-    if (!amountIsValid) {
-      return msg.channel.createMessage("Invalid donation amount");
-    }
-
-    return Promise.all([
-      msg.channel.createMessage(`${mention} paid $${amount.toFixed(2)}`),
-      updateMemberRoleForDonation(guild, member, amount),
-    ]);
-  },
+    bot.connect();
+  } catch (err) {
+    console.log("uh oh!", { err });
+  }
 };
 
-async function updateMemberRoleForDonation(guild, member, donationAmount) {
-  if (guild && member && donationAmount >= PREMIUM_CUTOFF) {
-    let role = Array.from(guild.roles.values()).find(
-      (role) => role.name === premiumRole.name
-    );
-    if (!role) {
-      role = await guild.createRole(premiumRole);
-    }
-
-    return member.addRole(role.id, "Donated $10 or more.");
-  }
-}
-
-// command handler, handles 'no matching command' case
-bot.on("messageCreate", async (msg) => {
-  const content = msg.content;
-  if (!content.startsWith(PREFIX)) {
-    return;
-  }
-
-  const parts = content
-    .split(" ")
-    .map((s) => s.trim())
-    .filter((s) => s);
-  const commandName = parts[0].substr(PREFIX.length);
-
-  const command = commandMap[commandName];
-  if (!command) {
-    return;
-  }
-
-  const authorIsBotOwner = msg.author.id === BOT_OWNER_ID;
-  if (command.botOwnerOnly && !authorIsBotOwner) {
-    return await msg.channel.createMessage(
-      `Hey, fuck you. You are ${msg.author.id}.`
-    );
-  }
-
-  const args = parts.slice(1);
-
-  try {
-    await command.execute(msg, args);
-  } catch (err) {
-    console.warn("Error handling command");
-    console.warn(err);
-  }
-});
-
-bot.on("error", (err) => {
-  console.warn(err);
-});
-
-bot.connect();
+runServer();
