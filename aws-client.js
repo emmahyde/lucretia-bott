e5399d8923b5cb17b367a77a10b2682cb9a2593a
kaylakantola <kkantola@trhc.com>
2020-05-26 11:59:10 -0400

    kk updates

--- aws-client.js
+++ aws-client.js
@@ -1,14 +1,24 @@
-const AWS = require('aws-sdk');
-AWS.config.update({region:'us-east-1'});
+const AWS = require("aws-sdk");
+AWS.config.update({ region: "us-east-1" });
 const ssm = new AWS.SSM();
 
 const getSecret = async (secretName) => {
   console.log(`Getting secret for ${secretName}`);
   const params = {
-    Name: secretName
-  }
-  const result = await ssm.getParameter(params).promise();
-  return result.Parameter.Value;
-}
+    Name: secretName,
+  };
+  try {
+    const result = await ssm.getParameter(params).promise();
+    console.log("before resolved:", { result }); // i would expect this to initially log as "undefined" because itll log before the promise resolves
 
-module.exports = { getSecret }
+    if (result) {
+      console.log("after resolved:", { result }); // i would expect this to have what you want
+      return result.Parameter.Value;
+    }
+  } catch (e) {
+    console.log("error from inside getSecret", { e });
+    throw new Error(e);
+  }
+};
+
+module.exports = { getSecret };
