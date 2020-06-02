b3772930e853a6a0758acf3f2228467652faad56
Hyde <emmajhyde@gmail.com>
2020-06-02 15:55:46 -0400

    help

--- server.js
+++ server.js
@@ -5,11 +5,9 @@ const { messageCreate } = require('./lib/message-create.js')
 
 async function resolveClient() {
   const token = await getParameterStore('lucretia-bott-token')
-
   const client = new Eris.Client(token)
 
   client.on('messageCreate', messageCreate)
-
   client.on('error', console.warn)
 
   client.connect()
