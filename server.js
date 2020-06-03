22cb728572a302e3bbc8a2aa21b6eb870d7b8eb2
Emma Hyde <emmajhyde@gmail.com>
2020-06-03 05:56:19 -0400

    enable cloudwatch maybe (#8)

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { messageCreate } = require('./lib/message-create.js')
 async function resolveClient() {
   const token = await getParameterStore('lucretia-bott-token')
   const client = new Eris.Client(token)
-
+  console.log('client created! about to listen...')
   client.on('messageCreate', messageCreate)
   client.on('error', console.warn)
 
