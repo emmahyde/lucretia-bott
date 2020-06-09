4da8215962d95107a802cf06a4f9772fa468c637
Hyde <emmajhyde@gmail.com>
2020-06-09 19:55:16 -0400

    new redis approach

--- deploy/start-service.sh
+++ deploy/start-service.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -xe
 source /home/ec2-user/.bash_profile
+sudo /etc/init.d/redis_6379 start
 cd /home/ec2-user/app/release
 npm run start
--- deploy/stop-service.sh
+++ deploy/stop-service.sh
@@ -4,3 +4,4 @@ source /home/ec2-user/.bash_profile
 cd /home/ec2-user/app/release && \
 npm run show
 npm stop
+/etc/init.d/redis_6379 stop
