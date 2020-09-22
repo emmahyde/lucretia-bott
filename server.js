5b586fcae9c51f716494213150630b10abda32c7
kaylakantola <kkantola@trhc.com>
2020-09-22 12:15:58 -0400

    one line ternary your highness

--- server.js
+++ server.js
@@ -5,9 +5,7 @@ const { messageCreate } = require('./lib/message-create.js')
 
 const { LOCAL, DISCORD_TOKEN } = process.env
 async function resolveClient() {
-  const token = LOCAL
-    ? DISCORD_TOKEN
-    : await getParameterStore('lucretia-bott-token')
+  const token = LOCAL ? DISCORD_TOKEN : await getParameterStore('lucretia-bott-token')
   const client = new Eris.Client(token)
   client.on('messageCreate', messageCreate)
   client.on('error', console.warn)
