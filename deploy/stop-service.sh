cd42dd8518eca714918de23feaeebffba5838ba4
Emma Hyde <emmajhyde@gmail.com>
2022-06-17 08:29:13 -0400

    stop service syntax for pm2

--- deploy/stop-service.sh
+++ deploy/stop-service.sh
@@ -3,7 +3,7 @@ source /home/ec2-user/.bash_profile
 [ -d "/home/ec2-user/app/release" ] && \
 cd /home/ec2-user/app/release && \
 npm run show
-pm2 save
+node ./node_modules/pm2/bin/pm2 save
 npm run show
 npm stop
 redis-cli shutdown
