ae01ed502b023d18e36e78078283019b1ba1d31b
Emma Hyde <emmajhyde@gmail.com>
2022-06-17 15:35:36 -0400

    .gitignore updates, comments, and cleanup

--- deploy/stop-old-service.sh
+++ deploy/stop-old-service.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -xe
+
 source /home/ec2-user/.bash_profile
 
 if [ -d "/home/ec2-user/app/release" ]; then
-	cd /home/ec2-user/app/release
+  cd /home/ec2-user/app/release
   node ./node_modules/pm2/bin/pm2 save
   npm run show
   npm stop
