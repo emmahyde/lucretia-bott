0d2e492aaddaa7091d6c95ee0c6739d1f96a13b8
Emma Hyde <emmajhyde@gmail.com>
2022-06-17 19:27:40 -0400

    check specifically for node modules

--- deploy/stop-old-service.sh
+++ deploy/stop-old-service.sh
@@ -2,7 +2,7 @@
 
 source /home/ec2-user/.bash_profile
 
-if [ -d "/home/ec2-user/app/release" ]; then
+if [ -d "/home/ec2-user/app/release/node_modules" ]; then
   cd /home/ec2-user/app/release
   node ./node_modules/pm2/bin/pm2 save
   npm run show
