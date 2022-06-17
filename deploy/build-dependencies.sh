3d10d01cc69479b27074c650d93be416b0d506d7
Emma Hyde <emmajhyde@gmail.com>
2022-06-17 13:24:04 -0400

    rearrange things a bit

---
+++ deploy/build-dependencies.sh
@@ -0,0 +1,12 @@
+# START
+echo "Setting up NodeJS Environment..."
+curl https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+# Dot source the files to ensure that variables are available within the current shell
+. /home/ec2-user/.nvm/nvm.sh
+. /home/ec2-user/.bashrc
+
+echo "environment setup complete. exiting install_script..."
+# Install NVM, NPM, Node.JS
+nvm install v16.15.1
+npm clean-install
--- deploy/start-service.sh
+++ deploy/start-service.sh
@@ -2,7 +2,5 @@
 source /home/ec2-user/.bash_profile
 sudo /etc/init.d/redis_6379 start
 cd /home/ec2-user/app/release
-nvm alias default 16
-nvm use 16
-npm clean-install
-npm run start
+npm rebuild
+npm start
